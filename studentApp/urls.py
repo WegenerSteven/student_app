e424886285de265b6ce166fc5e12d1d16c2885a1
Gilbert Ondonga Nyamberi <hunchovault@gmail.com>
2026-08-18 11:16:46 +0300

    Add templates files and wire forms

--- studentApp/urls.py
+++ studentApp/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('students/', include('student_app.urls')),
+    
+    # Makes student list the homepage
+    path('', RedirectView.as_view(url='/students/', permanent=False)),
 ]
--- student_app/views.py
+++ student_app/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.db.models import Q
+from django.shortcuts import get_object_or_404, redirect, render
 
-# Create your views here.
+from .forms import StudentForm
+from .models import Student
+
+
+# 1. LIST VIEW (With Search)
+def student_list(request):
+    query = request.GET.get('q', '')
+    if query:
+        students = Student.objects.filter(
+            Q(name__icontains=query) | Q(email__icontains=query)
+        )
+    else:
+        students = Student.objects.all().order_by('-created_at')
+        
+    return render(request, 'student_app/student_list.html', {
+        'students': students,
+        'query': query
+    })
+
+# 2. DETAIL VIEW
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'student_app/student_detail.html', {'student': student})
+
+# 3. CREATE VIEW
+def student_add(request):
+    if request.method == 'POST':
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            student = form.save()
+            messages.success(request, f"Student '{student.name}' was successfully added!")
+            return redirect('student_app:list')
+    else:
+        form = StudentForm()
+        
+    return render(request, 'student_app/student_form.html', {
+        'form': form,
+        'title': 'Add New Student',
+        'button_text': 'Save Student'
+    })
+
+# 4. UPDATE VIEW
+def student_edit(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f"Student '{student.name}' was successfully updated!")
+            return redirect('student_app:detail', pk=student.pk)
+    else:
+        form = StudentForm(instance=student)
+        
+    return render(request, 'student_app/student_form.html', {
+        'form': form,
+        'student': student,
+        'title': f'Edit Student: {student.name}',
+        'button_text': 'Update Student'
+    })
+
+# 5. DELETE VIEW
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == 'POST':
+        name = student.name
+        student.delete()
+        messages.success(request, f"Student '{name}' was deleted.")
+        return redirect('student_app:list')
+        
+    return render(request, 'student_app/student_confirm_delete.html', {'student': student})
